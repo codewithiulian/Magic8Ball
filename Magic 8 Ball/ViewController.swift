c765df6cb80194db3cb98c79fc4bf699d8c7f214
Iulian Oana <iulian.andreioana@gmail.com>
2019-07-26 19:50:54 +0100

    Adding image hooks and callbacks.

--- Magic 8 Ball/ViewController.swift
+++ Magic 8 Ball/ViewController.swift
@@ -9,12 +9,32 @@
 import UIKit
 
 class ViewController: UIViewController {
-    
+    // Declare an image name strings collection.
+    let ballArray = ["ball1", "ball2", "ball3", "ball4", "ball5"]
+    // Declare the index image number.
+    var ballIndexImage : Int = 0
+    // Declare the ui image view.
+    @IBOutlet weak var ballImage: UIImageView!
+    // On load.
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        // Get a ball image.
+        newBallImage()
     }
 
-
+    // Callback for 'Ask' button press.
+    @IBAction func askButtonPressed(_ sender: Any) {
+        // Get a new ball image.
+        newBallImage()
+    }
+    
+    // Randomly changes the image ball.
+    func newBallImage(){
+        // Get a random integer (0...4).
+        ballIndexImage = Int(arc4random_uniform(4))
+        // Assign it as the name of the UIImage 'image' property.
+        ballImage.image = UIImage(named: ballArray[ballIndexImage])
+    }
+    
 }
 
