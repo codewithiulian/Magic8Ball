d469d3b390468bb588e25505794487c79bb36830
Iulian Oana <iulian.andreioana@gmail.com>
2019-07-26 19:28:25 +0100

    Initial app setup

---
+++ Magic 8 Ball/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Magic 8 Ball
+//
+//  Created by Iulian Oana on 26/07/2019.
+//  Copyright © 2019 Iulian Oana. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
